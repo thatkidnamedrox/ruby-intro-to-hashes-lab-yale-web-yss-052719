d88bc8c4d4ece915058da3818cd81c5a12c13cd1
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-22 16:07:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ def monopoly_with_second_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
   monopoly = {}
   monopoly[:railroads] = {}
-  
+
   monopoly[:railroads][:pieces] = 4
   monopoly[:railroads][:names] = {}
   monopoly[:railroads][:rent_in_dollars] = {}
@@ -27,12 +27,12 @@ def monopoly_with_third_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
   monopoly = {}
   monopoly[:railroads] = {}
-  
+
   monopoly[:railroads][:pieces] = 4
   monopoly[:railroads][:names] = {}
   monopoly[:railroads][:rent_in_dollars] = {}
-  
-  monopoly[:railroads][:rent_in_dollars][:one_piece_owned] = 25 
+
+  monopoly[:railroads][:rent_in_dollars][:one_piece_owned] = 25
   monopoly[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
   monopoly[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
   monopoly[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
@@ -46,12 +46,12 @@ def monopoly_with_fourth_tier
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
   monopoly = {}
   monopoly[:railroads] = {}
-  
+
   monopoly[:railroads][:pieces] = 4
   monopoly[:railroads][:names] = {}
   monopoly[:railroads][:rent_in_dollars] = {}
-  
-  monopoly[:railroads][:rent_in_dollars][:one_piece_owned] = 25 
+
+  monopoly[:railroads][:rent_in_dollars][:one_piece_owned] = 25
   monopoly[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
   monopoly[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
   monopoly[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
@@ -59,5 +59,5 @@ def monopoly_with_fourth_tier
   monopoly[:railroads][:names][:pennsylvania_railroad] = {}
   monopoly[:railroads][:names][:b_and_o_railroad] = {}
   monopoly[:railroads][:names][:shortline] = {}
-  
+
 end
